5a781a779ab9e3b90735a6ce08eb459e51a33a7a
12Shivam <shivam03412@gmail.com>
2022-08-17 22:02:01 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
@@ -6,10 +7,17 @@ import { generatePalette } from './colorHelper';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Routes>
+        <Route path='/'>
+          <Route index={true} />
+          <Route path='palette/:id' />
+        </Route>
+        <Route path='*' element={<Navigate to='/' replace={true} />} />
+      </Routes>
     );
+    //   <div className='App'>
+    //     <Palette palette={generatePalette(seedColors[4])} />
+    //   </div>;
   }
 }
 
