f26bacf7ba504cde8bfc0dfdc558a9541a49989d
12Shivam <shivam03412@gmail.com>
2022-08-23 22:39:13 +0530

    Refactor Palette Styles

--- src/Palette.js
+++ src/Palette.js
@@ -4,7 +4,19 @@ import { generatePalette } from './colorHelper';
 import Navbar from './Navbar';
 import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFooter';
+import withStyles from 'react-jss';
 import './Palette.css';
+
+const styles = {
+  Palette: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  colors: {
+    height: '90%',
+  },
+};
 
 class Palette extends Component {
   constructor(props) {
@@ -28,6 +40,7 @@ class Palette extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     const { colors, paletteName, emoji } = this._palette;
     const paletteId = this.props.params.id;
     const { level, format } = this.state;
@@ -42,18 +55,18 @@ class Palette extends Component {
       />
     ));
     return (
-      <div className='Palette'>
+      <div className={classes.Palette}>
         <Navbar
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
           showingAllColors
         />
-        <div className='Palette-colors'>{colorBoxes}</div>
+        <div className={classes.colors}>{colorBoxes}</div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
 }
 
-export default withRouter(Palette);
+export default withStyles(styles)(withRouter(Palette));
