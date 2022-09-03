79367656237e642c449a58d7416f098627cbbf72
12Shivam <shivam03412@gmail.com>
2022-09-03 19:04:03 +0530

    Save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import seedColors from './seedColors';
 class App extends Component {
   constructor(props) {
     super(props);
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
     this.state = {
-      palettes: seedColors,
+      palettes: savedPalettes || seedColors,
     };
     this.savePalette = this.savePalette.bind(this);
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    // save palettes to local storage
+    window.localStorage.setItem(
+      'palettes',
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     const { palettes } = this.state;
