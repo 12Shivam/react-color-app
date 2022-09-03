8128c21deee454f4fe35639d89e9f6bdf9f4735f
12Shivam <shivam03412@gmail.com>
2022-09-03 22:35:02 +0530

    Make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,12 +15,11 @@ const style = {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: props => (props.showingFullPalette ? '20%' : '10%'),
+      height: props => (props.showingFullPalette ? '20%' : '33.333%'),
     },
-
     [sizes.down('md')]: {
       width: '50%',
-      height: props => (props.showingFullPalette ? '10%' : '10%'),
+      height: props => (props.showingFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -118,6 +117,9 @@ const style = {
       marginBottom: '0',
       padding: '1rem',
       textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '6rem',
+      },
     },
     '& p': {
       fontSize: '2rem',
